09666f27e1110ef4f243c70e875f8a65ff1386c1
donghun <doutine19@gmail.com>
2026-04-21 16:50:10 +0900

    블로그 API

---
+++ src/main/java/me/scpark/springdeveloper/controller/ExampleController.java
@@ -0,0 +1,14 @@
+package me.scpark.springdeveloper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        model.addAttribute("name", "홍길동");
+        return "example";
+    }
+}
