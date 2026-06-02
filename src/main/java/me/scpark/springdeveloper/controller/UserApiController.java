360e5ec1cf7b1e0af61d3fcae2fb032c3b3b370a
박동훈 <doutine19@gmail.com>
2026-06-02 18:28:10 +0900

    "feat: chapter8"

---
+++ src/main/java/me/scpark/springdeveloper/controller/UserApiController.java
@@ -0,0 +1,19 @@
+package me.scpark.springdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.scpark.springdeveloper.dto.AddUserRequest;
+import me.scpark.springdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request){
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
